2fd535428eeaf7c2fefb2899f5ad06d7cc5524c8
zachary <zacharyberdell@gmail.com>
2021-12-01 08:53:35 -0700

    part of the product model built out.

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,20 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true, 
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    }
   },
   {
     sequelize,
